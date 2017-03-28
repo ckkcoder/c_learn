12bb974833739ea0f4439f6bfb73f7a88d2d85c6
kevin <3056782267@qq.com>
2017-03-28 20:30:45 +0800

    who的学习

---
+++ who/utmplib.c
@@ -0,0 +1,57 @@
+/*	utmplib.c - function to buffer reads from utmp file
+ *	
+ *	function are
+ *		utmp_open( filename )
+ *		utmp_next()
+ *		utmp_close()
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <utmp.h>
+
+#define NRECS	16
+#define UTSIZE	(sizeof(struct utmp))
+#define NULLUT	((struct utmp *)NULL)
+
+static char utmpbuf[ NRECS *UTSIZE ];
+static int num_recs;
+static int cur_rec;
+static int fd_utmp = -1;
+
+int utmp_open( char *filename )
+{
+	fd_utmp = open( filename,O_RDONLY );
+	cur_rec = num_recs = 0;	
+	return fd_utmp;
+}
+
+struct utmp *utmp_next()
+{
+	struct utmp *recp;
+	if( fd_utmp == -1 )
+		return NULLUT;
+	if( cur_rec == num_recs && utmp_reload() == 0 )
+		return NULLUT;
+
+	recp = (struct utmp *)&utmpbuf[ cur_rec * UTSIZE ];
+	cur_rec++;
+	return recp;
+}
+
+int utmp_reload()
+{
+	int amt_read;
+	amt_read = read( fd_utmp, utmpbuf, UTSIZE * NRECS );
+	num_recs = amt_read / UTSIZE;
+	cur_rec = 0;
+	return num_recs;
+}
+
+void utmp_close()
+{
+	if( fd_utmp != -1 )
+		close(fd_utmp);
+}
